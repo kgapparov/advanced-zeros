22f86a0bb1daddd11ce25c254f967d8cf0b64fb5
khassangali Gapparov <hasangaly@gmail.com>
2019-03-05 12:42:24 +0600

    checked

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  zeroConditionNumber = getdevider(base);
+  count = 0;
+  for (j = zeroConditionNumber; (number/j) > 0; j=j*zeroConditionNumber){
+    count = count + number/j;
+  }
+  return Math.trunc(count);
+};
+
+function getdevider(k){
+  result =[];
+  for (var i = 2; i<k-1; i++){
+    if (k%i == 0){
+      result.push(i);
+    }
+  }
+  if (result.length==0){
+    return k;
+  } else{
+  return result[result.length/2];
+  }
+
 }
